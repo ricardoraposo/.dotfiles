d52ccc3c8c7586a9e322b4bdcc56709e6e219f72
ricardo <ricardorbxx1@gmail.com>
2022-06-23 17:58:00 -0300

    changed something

--- nvim/.config/nvim/lua/rick/colorscheme.lua
+++ nvim/.config/nvim/lua/rick/colorscheme.lua
@@ -4,13 +4,13 @@
 -- vim.cmd "highlight Normal guibg=none"
 
 -- As things should be in gruvbox
-vim.opt.background = "dark"
-vim.cmd "colorscheme gruvbox"
+-- vim.opt.background = "dark"
+-- vim.cmd "colorscheme gruvbox"
 -- vim.cmd "highlight LineNr guifg=#ffffff"
 -- vim.cmd "highlight Normal guibg=none"
 
 -- As things should be in tokyonight
--- vim.g.tokyonight_style = "storm"
--- vim.cmd "colorscheme tokyonight"
+vim.g.tokyonight_style = "night"
+vim.cmd "colorscheme tokyonight"
 -- vim.cmd "highlight LineNr guifg=#ffffff"
 -- vim.cmd "highlight Normal guibg=none"
