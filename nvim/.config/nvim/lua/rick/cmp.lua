866731b71a361448509b7a8de9134b3ad6b8ef78
ricardo <ricardorbxx1@gmail.com>
2022-06-25 09:04:37 -0300

    changed something

--- nvim/.config/nvim/lua/rick/cmp.lua
+++ nvim/.config/nvim/lua/rick/cmp.lua
@@ -64,7 +64,7 @@ cmp.setup({
     { name = "cmp_tabnine", max_item_count = 3 },
     { name = "nvim_lua" },
     { name = "luasnip"},
-    { name = "buffer", keyword_length = 5 },
+    { name = "buffer", keyword_length = 8 },
     { name = "path" },
   },
   confirm_opts = {
