d63dce95ea2838cd78f179561aa3bfc4d89a7c87
ricardo <ricardorbxx1@gmail.com>
2022-06-28 13:47:05 -0300

    added a snippet to js/ts

---
+++ nvim/.config/nvim/lua/rick/snippets/typescript.lua
@@ -0,0 +1,34 @@
+local ls = require("luasnip")
+local fmt = require("luasnip.extras.fmt").fmt
+local t = ls.text_node
+local i = ls.insert_node
+local c = ls.choice_node
+local s = ls.s
+local rep = require("luasnip.extras").rep
+
+ls.add_snippets("typescript", {
+	s("req", fmt("const {} = require('{}')", { i(1, "default"), rep(1) })),
+	s(
+		"afb",
+		fmt(
+			[[
+    const {1} = ({2}) => {{
+      {3}
+    }}
+    ]],
+			{ i(1), i(2), i(3) }
+		)
+	),
+	s(
+		"afc",
+		fmt(
+			[[
+    ({1}) => {{
+      {2}
+    }}
+    ]],
+      { c(1, { t{""}, t{"req, res"}, }), i(2)}
+		)
+	),
+	s("cl", fmt("console.log({})", { i(1) })),
+})
