f74ee28f47a9874d7c675cb9b1335c1f82460a5f
ricardo <ricardorbxx1@gmail.com>
2022-07-15 22:38:49 -0300

    added go snippets

---
+++ nvim/.config/nvim/lua/rick/snippets/go.lua
@@ -0,0 +1,17 @@
+local ls = require('luasnip')
+local fmt = require("luasnip.extras.fmt").fmt
+local i = ls.insert_node
+local s = ls.s
+-- local rep = require("luasnip.extras").rep
+
+ls.add_snippets("go", {
+  s("fn", fmt(
+    [[
+    func {1}({2}) {3} {{
+      {4}
+    }}
+    ]],
+    { i(1), i(2), i(3),i(4) }
+  ))
+})
+
