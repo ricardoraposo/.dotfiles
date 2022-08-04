d8426ee86cedfe572dde99a4415dd154beaf0991
ricardo <ricardorbxx1@gmail.com>
2022-08-04 16:33:22 -0300

    removed emmet for jsx files

---
+++ nvim/.config/nvim/lua/rick/lsp/settings/emmet_ls.lua
@@ -0,0 +1,18 @@
+return {
+	filetypes = {
+		"html",
+		"css",
+		"scss",
+		"haml",
+		"xml",
+		"xsl",
+		"pug",
+		"slim",
+		"sass",
+		"stylus",
+		"less",
+		"sss",
+		"hbs",
+		"handlebars",
+	},
+}
