c34103b7fa719088d3f351c15a0f804a9f3f9d08
Ricardo Raposo <ricardorbxx1@gmail.com>
2022-08-09 21:02:22 -0300

    had to change the color config bc the plugin creator is a dumbass

--- nvim/.config/nvim/lua/rick/colorscheme.lua
+++ nvim/.config/nvim/lua/rick/colorscheme.lua
@@ -1,35 +1,32 @@
-local status_ok, color = pcall(require, "doom-one")
-if not status_ok then
-	return
-end
+-- Add color to cursor
+vim.g.doom_one_cursor_coloring = false
+-- Set :terminal colors
+vim.g.doom_one_terminal_colors = true
+-- Enable italic comments
+vim.g.doom_one_italic_comments = false
+-- Enable TS support
+vim.g.doom_one_enable_treesitter = true
+-- Color whole diagnostic text or only underline
+vim.g.doom_one_diagnostics_text_color = false
+-- Enable transparent background
+vim.g.doom_one_transparent_background = true
 
-color.setup({
-	cursor_coloring = false,
-	terminal_color = true,
-	enable_treesitter = true,
-	italic_comments = true,
-	transparent_background = true,
-	pumblend = {
-		enable = false,
-		transparency_amount = 100,
-	},
-	plugins_integrations = {
-		gitsigns = true,
-		telescope = true,
-		neogit = true,
-		nvim_tree = true,
-	},
-})
+-- Plugins integration
+vim.g.doom_one_plugin_telescope = false
+vim.g.doom_one_plugin_neogit = true
+vim.g.doom_one_plugin_nvim_tree = true
+
+vim.cmd("colorscheme doom-one")
 
 vim.api.nvim_set_hl(0, "lineNr", {
-	fg = "#41a7fc",
+  fg = "#41a7fc",
 })
 
 vim.api.nvim_set_hl(0, "FloatermBorder", {
-	bg = "none",
-	fg = "#41a7fc",
+  bg = "none",
+  fg = "#41a7fc",
 })
 
 vim.api.nvim_set_hl(0, "Floaterm", {
-	bg = "none",
+  bg = "none",
 })
