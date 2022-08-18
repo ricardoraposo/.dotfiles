f877fdf07286b2e7c60d558f2bb448d077a73506
Ricardo Raposo <ricardorbxx1@gmail.com>
2022-08-18 10:51:33 -0300

    i have no idea what i've done

--- nvim/.config/nvim/lua/rick/plugins.lua
+++ nvim/.config/nvim/lua/rick/plugins.lua
@@ -46,6 +46,7 @@ return packer.startup(function(use)
   use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
   use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
   use("akinsho/toggleterm.nvim")
+
   -- Greatest colorscheme ever created
   use("ricardoraposo/benjamin.nvim")
 
