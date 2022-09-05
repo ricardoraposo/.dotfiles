c6c5d4341501990849b10080702a1f933f999870
ricardo <ricardorbxx1@gmail.com>
2022-09-05 09:22:01 -0300

    stuff

--- nvim/.config/nvim/lua/rick/lsp/handlers.lua
+++ nvim/.config/nvim/lua/rick/lsp/handlers.lua
@@ -56,9 +56,7 @@ local function lsp_keymaps(bufnr)
 end
 
 M.on_attach = function(client, bufnr)
-	if client.name == "tsserver" then
-		client.server_capabilities.document_formatting = false
-	end
+	client.server_capabilities.document_formatting = false
 	lsp_keymaps(bufnr)
 end
 
--- nvim/.config/nvim/lua/rick/toggleterm.lua
+++ nvim/.config/nvim/lua/rick/toggleterm.lua
@@ -13,7 +13,7 @@ toggleterm.setup({
     start_in_insert = true,
     insert_mappings = true,
     persist_size = true,
-    direction = "float",
+    direction = "horizontal",
     close_on_exit = true,
     shell = vim.o.shell,
     float_opts = {
@@ -26,10 +26,10 @@ toggleterm.setup({
 function _G.set_terminal_keymaps()
     local opts = { noremap = true, silent = true }
     keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
-    keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
-    keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-    keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
-    keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+    keymap(0, "t", "<C-w>h", [[<C-\><C-n><C-W>h]], opts)
+    keymap(0, "t", "<C-w>j", [[<C-\><C-n><C-W>j]], opts)
+    keymap(0, "t", "<C-w>k", [[<C-\><C-n><C-W>k]], opts)
+    keymap(0, "t", "<C-w>l", [[<C-\><C-n><C-W>l]], opts)
 end
 
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
