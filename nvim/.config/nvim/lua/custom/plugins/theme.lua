daa454820d22473f941afa145765908b17211955
ricardo <ricardorbxx1@gmail.com>
2025-08-10 18:32:41 -0300

    pop rules

--- nvim/.config/nvim/lua/custom/plugins/theme.lua
+++ nvim/.config/nvim/lua/custom/plugins/theme.lua
@@ -1,40 +1,60 @@
 return {
-  -- 'nvim-lua/plenary.nvim',
-  -- { 'nvim-tree/nvim-web-devicons', lazy = true },
-  --
-  -- {
-  --   'nvchad/ui',
-  --   config = function()
-  --     require 'nvchad'
-  --   end,
-  -- },
-  --
-  -- {
-  --   'nvchad/base46',
-  --   lazy = true,
-  --   build = function()
-  --     require('base46').load_all_highlights()
-  --   end,
-  -- },
-  --
-  -- 'nvchad/volt',
-
   { 'tjdevries/colorbuddy.nvim' },
   {
     'ellisonleao/gruvbox.nvim',
     opts = {
       bold = false,
+      transparent_mode = true,
+      italic = {
+        strings = false,
+        emphasis = false,
+        comments = false,
+        operators = false,
+        folds = false,
+      },
       contrast = 'hard',
     },
   },
   { 'HoNamDuong/hybrid.nvim' },
   { 'rebelot/kanagawa.nvim' },
   { 'cocopon/iceberg.vim' },
-  { 'ricardoraposo/gruvbox-minor.nvim' },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    opts = {
+      no_italic = true,
+      no_bold = true,
+    },
+  },
+  { 'nickkadutskyi/jb.nvim' },
   {
     'ricardoraposo/nightwolf.nvim',
     opts = {
+      theme = 'dark-gray',
       italic = false,
-    }
+    },
   },
+  { 'Mofiqul/vscode.nvim' },
+  { 'joshdick/onedark.vim' },
 }
+
+-- 'nvim-lua/plenary.nvim',
+-- { 'nvim-tree/nvim-web-devicons', lazy = true },
+--
+-- {
+--   'nvchad/ui',
+--   config = function()
+--     require 'nvchad'
+--   end,
+-- },
+--
+-- {
+--   'nvchad/base46',
+--   lazy = true,
+--   build = function()
+--     require('base46').load_all_highlights()
+--   end,
+-- },
+--
+-- 'nvchad/volt',
