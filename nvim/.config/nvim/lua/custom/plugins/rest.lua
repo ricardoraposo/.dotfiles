f4cab68cf11f61e4b3ac99539e96f54607d98dda
ricardo <ricardorbxx1@gmail.com>
2026-01-14 11:09:39 -0300

    this shit sucks to setup, commented out for now

--- nvim/.config/nvim/lua/custom/plugins/rest.lua
+++ nvim/.config/nvim/lua/custom/plugins/rest.lua
@@ -1,10 +1,11 @@
 return {
-  'rest-nvim/rest.nvim',
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter',
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      table.insert(opts.ensure_installed, 'http')
-    end,
-  },
+  -- 'rest-nvim/rest.nvim',
+  -- dependencies = {
+  --   'nvim-treesitter/nvim-treesitter',
+  --   'nvim-neotest/nvim-nio',
+  --   opts = function(_, opts)
+  --     opts.ensure_installed = opts.ensure_installed or {}
+  --     table.insert(opts.ensure_installed, 'http')
+  --   end,
+  -- },
 }
