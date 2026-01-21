ed54d5ce504fcb24df3973764f7896a0ac8aa468
ricardo <ricardorbxx1@gmail.com>
2026-01-21 14:38:36 -0300

    add opencode and gh

--- setup-fedora.sh
+++ setup-fedora.sh
@@ -100,3 +100,11 @@ cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/appli
 sed -i "s|Icon=kitty|Icon=$(readlink -f ~)/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
 sed -i "s|Exec=kitty|Exec=$(readlink -f ~)/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
 echo 'kitty.desktop' > ~/.config/xdg-terminals.list
+
+# opencode
+curl -fsSL https://opencode.ai/install | bash
+
+# gh cli
+sudo dnf install dnf5-plugins
+sudo dnf config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo
+sudo dnf install gh --repo gh-cli
