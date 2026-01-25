8154e29e2a1ff3b036e24e9edbec43af80bf21d5
ricardo <ricardorbxx1@gmail.com>
2026-01-25 16:15:51 -0300

    feat: add yazi to setup script

--- setup-fedora.sh
+++ setup-fedora.sh
@@ -4,11 +4,12 @@ cd ~/
 
 # install some nice deps
 sudo dnf copr enable dejan/lazygit
+sudo dnf copr enable lihaohong/yazi
 
 sudo dnf -y install \
        ninja-build cmake gcc gcc-c++ make autoconf \
        gettext curl glibc-gconv-extra git zsh stow fzf wget difftastic fd-find \
-       openssl-devel ncurses-devel wxGTK-devel lazygit pass pass-otp
+       openssl-devel ncurses-devel wxGTK-devel lazygit pass pass-otp yazi
 
 # install go manually
 wget https://go.dev/dl/go1.25.6.linux-amd64.tar.gz
