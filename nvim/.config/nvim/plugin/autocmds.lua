d477334bf004625091f47df38147c9e1e5f2190a
ricardo <ricardorbxx1@gmail.com>
2026-03-04 17:39:44 -0300

    feat: add autocmd to open todo list on startup

--- nvim/.config/nvim/plugin/autocmds.lua
+++ nvim/.config/nvim/plugin/autocmds.lua
@@ -1,3 +1,13 @@
+vim.api.nvim_create_autocmd('VimEnter', {
+  group = vim.api.nvim_create_augroup('open-todo', { clear = true }),
+  callback = function()
+    local args = vim.fn.argv()
+    if #args == 0 then
+      vim.cmd('edit ~/Projects/notes/todo.md')
+    end
+  end,
+})
+
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
