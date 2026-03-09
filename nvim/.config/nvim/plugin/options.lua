4acdf2569f34233b9b3a446681387efa5eb4705d
ricardo <ricardorbxx1@gmail.com>
2026-03-09 13:55:57 -0300

    tokyooooo

--- nvim/.config/nvim/plugin/options.lua
+++ nvim/.config/nvim/plugin/options.lua
@@ -38,4 +38,4 @@ vim.opt.showmode = false
 -- vim.opt.ru = true
 -- vim.opt.ruf = "%80(%=  %#@variable#%.50F %m[%{&ft}]%)"
 
-vim.cmd.colorscheme 'cursor-dark'
+vim.cmd.colorscheme 'tokyonight-night'
