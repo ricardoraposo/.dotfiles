95db8e21689ff3c349ee18e0e1bc58f598a5561b
ricardo <ricardorbxx1@gmail.com>
2026-07-13 20:57:31 -0300

    feat: added keymaps and initial plugins

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,35 +1,26 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- vim.g.base46_cache = vim.fn.stdpath 'data' .. '/base46_cache/'
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
+vim.g.have_nerd_font = true
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1
+vim.g.omni_sql_no_default_maps = 1
 
--- NOTE: Here is where you install your plugins.
-require('lazy').setup({
-  {
-    import = 'custom.plugins',
-  },
-  {
-    import = 'rick.plugins.gitsigns',
-  },
-  rocks = {
-    enable = false,
-  }
-}, {
-  change_detection = {
-    notify = false,
-  },
-})
--- dofile(vim.g.base46_cache .. 'defaults')
---
--- dofile(vim.g.base46_cache .. 'treesitter')
--- dofile(vim.g.base46_cache .. 'colors')
--- dofile(vim.g.base46_cache .. 'syntax')
+require 'config'
+
+vim.pack.add {
+  'https://github.com/ember-theme/nvim',
+  'https://github.com/stevearc/oil.nvim',
+  'https://github.com/neovim/nvim-lspconfig',
+  'https://github.com/christoomey/vim-tmux-navigator',
+}
+
+-- oil
+require('oil').setup()
+
+-- herdr navigator
+vim.g.tmux_navigator_no_mappings = 1
+dofile(vim.fn.expand '~/.config/herdr/plugins/config/vim-herdr-navigation/vim-herdr-navigation/editor/nvim.lua')
+
+-- theme
+vim.cmd.colorscheme 'ember'
---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,59 @@
+local map = vim.keymap.set
+
+map('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+map('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+map('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+map('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+map('t', '<esc>', '<C-\\><C-n>')
+map('n', '<leader>rt', ':15split | terminal ')
+
+map('i', '<C-c>', '<Esc>')
+map('n', '<leader><leader>', '<C-^>', { desc = 'Alternate files' })
+map('v', '<', '<gv')
+map('v', '>', '>gv')
+map('n', '<leader>o', 'ddO', { desc = 'Insert in current line, with correct identation' })
+map('n', '<leader>do', ':DBUIToggle<CR>', { desc = 'Toggle DBUI' })
+
+map('v', '<S-j>', ':m .+1<CR>==')
+map('v', '<S-k>', ':m .-2<CR>==')
+map('x', 'J', ":move '>+1<CR>gv-gv")
+map('x', 'K', ":move '<-2<CR>gv-gv")
+
+map('n', '<F1>', '<cmd>set nu!<CR>')
+map('n', '<F2>', '<cmd>set rnu!<CR>')
+map('n', '<F3>', '<cmd>set cursorcolumn!<CR>')
+map('n', '<F4>', '<cmd>set cursorline!<CR>')
+map('n', '<F5>', '<cmd>set wrap!<CR>')
+
+map('n', '<leader>tf', '!!turnFunction<CR>')
+map('n', '<leader>p', 'i|>')
+
+map('n', '<M-Up>', '<cmd>resize +2<CR>')
+map('n', '<M-Down>', '<cmd>resize -2<CR>')
+map('n', '<M-Left>', '<cmd>vertical resize +2<CR>')
+map('n', '<M-Right>', '<cmd>vertical resize -2<CR>')
+
+map('n', '<S-Up>', '<cmd>resize +2<CR>')
+map('n', '<S-Down>', '<cmd>resize -2<CR>')
+map('n', '<S-Left>', '<cmd>vertical resize +2<CR>')
+map('n', '<S-Right>', '<cmd>vertical resize -2<CR>')
+
+map('n', '<leader>vv', '<cmd>vsplit<CR>')
+map('n', '<leader>v-', '<cmd>split<CR>')
+map('n', '<leader>vt', '<cmd>vsplit | terminal<CR>')
+
+map('n', 'G', 'Gzz')
+map('n', '<C-d>', '<C-d>zz')
+map('n', '<C-u>', '<C-u>zz')
+map('n', 'Y', 'y$')
+map('n', '<leader><', '0f>ldt<')
+map('n', '<leader>so', '<cmd>so %<cr>')
+
+map('n', '<leader>n', '<cmd>Oil<CR>')
+
+vim.cmd 'command! Wq wq'
+vim.cmd 'command! W w'
+vim.cmd 'command! Q q'
